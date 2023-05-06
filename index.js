e23f0da25119f0527076d399262c4d19772219eb
Cenk Erdonmez <cenkerdonmez@gmail.com>
2023-05-07 00:54:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+ return ( fetch("https://anapioficeandfire.com/api/books")
+  .then(function(response){
+    return response.json()
+  })
+  .then(function(json){
   
+    renderBooks(json)
+    return json
+  }))
 }
 
 function renderBooks(books) {
